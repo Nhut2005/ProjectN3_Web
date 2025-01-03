c8b06ccaaaca3735a70e7e97a827896fb31e4308
TuyetLoan02 <nguyenvutuyetloan@gmail.com>
2025-01-03 14:55:49 +0700

    Create ERD Database

--- Transport/Transport/settings.py
+++ Transport/Transport/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'T_package_Mng',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Transport.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DjangoDB',
+        'USER': 'root',
+        'PASSWORD': 'my-secret-pw',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
