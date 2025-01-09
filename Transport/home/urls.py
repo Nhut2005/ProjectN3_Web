ef02c6518a7c35183e57e5b62d2b67f45dd1bf09
TuyetLoan02 <nguyenvutuyetloan@gmail.com>
2025-01-09 21:08:58 +0700

    Tao edit create-package

--- Transport/home/urls.py
+++ Transport/home/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),  
     path('package', views.package, name="package"),
-    path('edit-package', views.edit_package, name="edit-package"),
+    path('create-package', views.edit_package, name="create-package"),
 ]
